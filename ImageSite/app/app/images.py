178fec30a7a2913b40e0a3e800b8e0ca1933f6a9
TahaAMalik <tahaamalik101@gmail.com>
2026-04-25 20:30:52 -0400

    Update images.py

--- ImageSite/app/app/images.py
+++ ImageSite/app/app/images.py
@@ -1,6 +1,7 @@
-from imagekitio import AsyncImageKit
 import os
+
 from dotenv import load_dotenv
+from imagekitio import AsyncImageKit
 
 load_dotenv()
 
@@ -8,4 +9,4 @@ imagekit = AsyncImageKit(
     private_key=os.getenv("IMAGEKIT_PRIVATE_KEY"),
 )
 
-url_endpoint = os.getenv("IMAGEKIT_URL_ENDPOINT")
+url_endpoint = os.getenv("IMAGEKIT_URL")
